3628beed9fed7e698a7918ae58de719015230cf0
dulanovic <vidan134@live.com>
2018-12-03 02:36:13 +0100

    Stack implementation split into 2 files

--- src/stack.h
+++ src/stack.h
@@ -4,17 +4,22 @@
 
 #include <stddef.h>
 
-struct Stack {
-    double *items;
-    size_t last;
-    size_t length;
-};
+typedef struct Stack *pStack;
 
-struct Stack * new(void);
+/* struct Stack * new(void);
 void freeStack(struct Stack *stack);
 int push(struct Stack *stack, double item);
 double pop(struct Stack *stack);
 int isEmpty(struct Stack *stack);
-void printStack(struct Stack *stack);
+void printStack(struct Stack *stack); */
+
+pStack new(void);
+void freeStack(pStack stack);
+int push(pStack stack, double item);
+double pop(pStack stack);
+int isEmpty(pStack stack);
+void printStack(pStack stack);
+
+
 
 #endif
--- src/stack_array.c
+++ src/stack_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "stack_array.h"
+#include "stack.h"
 
 struct Stack {
     double *items;
